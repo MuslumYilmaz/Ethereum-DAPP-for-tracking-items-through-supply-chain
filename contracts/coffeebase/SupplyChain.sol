dfacca21392dfe588877f9274bde626de85c20bd
MuslumYilmaz <muslum@vizyon.ai>
2021-07-08 11:20:57 +0300

    pack function and test implemented

--- contracts/coffeebase/SupplyChain.sol
+++ contracts/coffeebase/SupplyChain.sol
@@ -96,7 +96,7 @@ contract SupplyChain {
 
   // Define a modifier that checks if an item.state of a upc is Processed
   modifier processed(uint _upc) {
-
+    require(items[_upc].itemState == State.Processed);
     _;
   }
   
@@ -182,14 +182,14 @@ contract SupplyChain {
   // Define a function 'packItem' that allows a farmer to mark an item 'Packed'
   function packItem(uint _upc) public 
   // Call modifier to check if upc has passed previous supply chain stage
-  
+  processed(_upc)
   // Call modifier to verify caller of this function
-  
+  onlyOwner
   {
     // Update the appropriate fields
-    
+    items[_upc].itemState = State.Packed;
     // Emit the appropriate event
-    
+    emit Packed(_upc);
   }
 
   // Define a function 'sellItem' that allows a farmer to mark an item 'ForSale'
