ef643b19386f840f03e94d58478c21803f196928
MuslumYilmaz <muslum@vizyon.ai>
2021-07-09 13:28:21 +0300

    test fixes

--- test/TestSupplychain.js
+++ test/TestSupplychain.js
@@ -14,7 +14,7 @@ contract('SupplyChain', function(accounts) {
     const originFarmLongitude = "144.341490"
     var productID = sku + upc
     const productNotes = "Best beans for Espresso"
-    const productPrice = web3.utils.toWei('1', 'ether');
+    const productPrice = web3.toWei(1, "ether");
     var itemState = 0
     const distributorID = accounts[2]
     const retailerID = accounts[3]
@@ -265,7 +265,7 @@ contract('SupplyChain', function(accounts) {
         var eventEmitted = false
         
         // Watch the emitted event Purchased()
-        var event = supplyChain.Received({}, (err, event) => {          eventEmitted = true        })
+        var event = supplyChain.Purchased({}, (err, event) => {          eventEmitted = true        })
 
         //add consumer
         await supplyChain.addConsumer(consumerID);
