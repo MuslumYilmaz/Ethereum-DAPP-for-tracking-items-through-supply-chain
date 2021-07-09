28aba604524421417820f59db8a736a48e486073
MuslumYilmaz <muslum@vizyon.ai>
2021-07-09 14:28:39 +0300

    deploy to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,22 @@
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "0e7faeaa9e3846759d7780ec4bd35913";
+//
+// const fs = require('fs');
+const mnemonic = "tortoise surface refuse leisure believe peasant magic slab glance design pride collect";
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    },
   },
   compilers: {
     solc: {
